957d6ca5efa1a808d360b15bc0f76bdcf4990168
Anselme <anselmedupuy@gmail.com>
2026-09-16 11:58:57 +0200

    red: 6 tests, all failed

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -5,3 +5,15 @@ def check_duration_free_parking(duration_in_minutes: float, is_electric: bool) -
 
 def calculate_parking_fee(duration_in_minutes: float, subscribed: bool, is_electric: bool) -> float:
     return None
+
+def price_cap(price: float, duration_in_minutes: int, subscribed: bool) -> bool:
+    return None
+
+def impound(duration_in_minutes: int) -> bool:
+    return None
+
+def check_duration(time_arrive: datetime, time_departure: datetime) -> int:
+    return None
+
+def check_price_at_time(price: float, duration_in_minutes: int, subscribed: bool) -> bool:
+    return None
