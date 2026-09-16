4401f777800772e23a91403b47a7b617669d8fcd
Anselme <anselmedupuy@gmail.com>
2026-09-16 11:58:00 +0200

    red: 6 tests, all failed

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -2,3 +2,6 @@ from datetime import datetime
 
 def check_duration_free_parking(duration_in_minutes: float, is_electric: bool) -> bool:
     return None
+
+def calculate_parking_fee(duration_in_minutes: float, subscribed: bool, is_electric: bool) -> float:
+    return None
